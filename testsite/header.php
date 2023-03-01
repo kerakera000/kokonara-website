f2a362e4051459d4ac036ac0b4bec222d1e5d114
kerakera000 <leon51414@icloud.com>
2023-03-02 01:06:12 +0900

    セーブ

--- testsite/header.php
+++ testsite/header.php
@@ -11,8 +11,8 @@
 
 		<link rel="preconnect" href="https://fonts.googleapis.com">
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-		<link href="https://fonts.googleapis.com/css2?family=Kosugi+Maru&family=Roboto&display=swap" rel="stylesheet">
-		
+		<link href="https://fonts.googleapis.com/css2?family=Kosugi+Maru&family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+
 		<title><?php wp_title(); ?> test site</title>
 
     <?php wp_head(); ?>
--- testsite/home.php
+++ testsite/home.php
@@ -16,8 +16,12 @@
 
 <section class="about">
     <div class="about__img-box">
-        <img class="about__img-box--light" src="<?php echo get_stylesheet_directory_uri(); ?>/asset/images/about-img/about-light.png" alt="">
-        <img class="about__img-box--light-back" src="<?php echo get_stylesheet_directory_uri(); ?>/asset/images/about-img/about-light-back.png" alt="">
+        <div class="about__img-box--light">
+            <img class="about__img-box--light--img" src="<?php echo get_stylesheet_directory_uri(); ?>/asset/images/about-img/about-light.png" alt="">
+        </div>
+        <div class="about__img-box--light-back">
+            <img class="about__img-box--light-back--img" src="<?php echo get_stylesheet_directory_uri(); ?>/asset/images/about-img/about-light-back.png" alt="">
+        </div>
     </div>
     <div class="about__text-box">
         <h2 class="about__text-box--title">「わかりやすさ」と「デザイン」で<br> お客様目線に立ったものをつくる</h2>
@@ -29,7 +33,218 @@
     </div>
 </section>
 
+<section class="service">
+    <div class="service__title">
+        <div class="title">
+            <h2 class="title__main-text">事業内容</h2>
+            <div class="title__line"></div>
+            <p class="title__sub-text">SERVICE</p>
+        </div>
+    </div>
+    <div class="service__web">
+        <div class="service__web--title">
+            <h3 class="service__web--title--text">WEB制作</h3>
+        </div>
+        <div class="service__web--content">
+            <div class="web">
+                <div class="web__title">
+                    <div class="web__title--cnt">
+                        <h4 class="web__title--cnt--text">Service</h4>
+                        <span class="web__title--cnt--num">01</span>
+                    </div>
+                    <div class="web__title--line"></div>
+                </div>
 
+                <div class="web__body">
+                    <div class="display-content">
+                        <div class="web__body--title">
+                            <h5 class="web__body--title--text">ワイヤーフレーム / デザイン案</h5>
+                        </div>
+                        <div class="web__body--text-box">
+                            <p class="web__body--text-box--text">
+                                お客様からのヒアリングをもとに(設計図)を作成した後デザインを作成します。デザインはTOP+4ページでスマホ版も作成します。 画像と文章はお客様が用意してくださることが望ましいですが、こちらで用意することも可能です。
+                            </p>
+                        </div>
+                    </div>
+                    <div class="web__body--img-cnt">
+                        <div class="web__body--img-cnt--box">
+                            <img class="web__body--img-cnt--box--img web-img-1" src="<?php echo get_stylesheet_directory_uri(); ?>/asset/images/service-img/service1.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="web">
+                <div class="web__title">
+                    <div class="web__title--cnt">
+                        <h4 class="web__title--cnt--text">Service</h4>
+                        <span class="web__title--cnt--num">02</span>
+                    </div>
+                    <div class="web__title--line"></div>
+                </div>
 
+                <div class="web__body">
+                    <div class="display-content">
+                        <div class="web__body--title">
+                            <h5 class="web__body--title--text">サイト構築</h5>
+                        </div>
+                        <div class="web__body--text-box">
+                            <p class="web__body--text-box--text">
+                                作成したデザインを元にサイトを構築して、デザインを元に適切なアニメーションや機能を採用します。 お客様の事業の魅力が伝わりやすく、且つ使いやすいホームぺージを作っていきます。
+                            </p>
+                        </div>
+                    </div>
+                    <div class="web__body--img-cnt">
+                        <div class="web__body--img-cnt--box">
+                            <img class="web__body--img-cnt--box--img web-img-2" src="<?php echo get_stylesheet_directory_uri(); ?>/asset/images/service-img/service2.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="web">
+                <div class="web__title">
+                    <div class="web__title--cnt">
+                        <h4 class="web__title--cnt--text">Service</h4>
+                        <span class="web__title--cnt--num">03</span>
+                    </div>
+                    <div class="web__title--line"></div>
+                </div>
 
+                <div class="web__body">
+                    <div class="display-content">
+                        <div class="web__body--title">
+                            <h5 class="web__body--title--text">サイトリニューアル</h5>
+                        </div>
+                        <div class="web__body--text-box">
+                            <p class="web__body--text-box--text">
+                            現状のサイトの問題をヒアリングで洗い出し、お客様の求めるゴールを一緒に解決できるよう、ホームページを作っていきます。ページの増設やレイアウトの変更など問題点を分析し、お客様と解決していきます。
+                            </p>
+                        </div>
+                    </div>
+                    <div class="web__body--img-cnt">
+                        <div class="web__body--img-cnt--box">
+                            <img class="web__body--img-cnt--box--img web-img-3" src="<?php echo get_stylesheet_directory_uri(); ?>/asset/images/service-img/service3.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="web">
+                <div class="web__title">
+                    <div class="web__title--cnt">
+                        <h4 class="web__title--cnt--text">Service</h4>
+                        <span class="web__title--cnt--num">04</span>
+                    </div>
+                    <div class="web__title--line"></div>
+                </div>
+
+                <div class="web__body">
+                    <div class="display-content">
+                        <div class="web__body--title">
+                            <h5 class="web__body--title--text">充実のサポート</h5>
+                        </div>
+                        <div class="web__body--text-box">
+                            <p class="web__body--text-box--text">
+                            サービス購入時から、納品後までサポートが充実しています。サイト構築に必要な文章や画像、サイトを表示する際に必要になるサーバーやドメインの取得や設定WordPressの初期設定もすべて対応可能です。納品後はトラブル対応や、修正などの定期更新も承っております。
+                            </p>
+                        </div>
+                    </div>
+                    <div class="web__body--img-cnt">
+                        <div class="web__body--img-cnt--box">
+                            <img class="web__body--img-cnt--box--img web-img-4" src="<?php echo get_stylesheet_directory_uri(); ?>/asset/images/service-img/service4.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="flow">
+    <div class="flow__title">
+        <div class="title">
+            <h2 class="title__main-text">制作フロー</h2>
+            <div class="title__line"></div>
+            <p class="title__sub-text">FLOW</p>
+        </div>
+    </div>
+    <div class="flow__cnt">
+        <div class="flow-cnt">
+            <div class="flow-cnt__box">
+                <div class="flow-cnt__box--img-box">
+                    <div class="flow-cnt__box--img-box--size">
+                        <img class="flow-cnt__box--img-box--size--img" src="<?php echo get_stylesheet_directory_uri(); ?>/asset/images/flow-img/flow1.png" alt="">
+                    </div>
+                </div>
+                <div class="flow-cnt__box--text-box">
+                    <p class="flow-cnt__box--text-box--text">「見積もり・カスタマイズの相談」からお問い合わせください</p>
+                </div>
+            </div>
+            <div class="flow-cnt__box">
+                <div class="flow-cnt__box--img-box">
+                    <div class="flow-cnt__box--img-box--size">
+                        <img class="flow-cnt__box--img-box--size--img" src="<?php echo get_stylesheet_directory_uri(); ?>/asset/images/flow-img/flow2.png" alt="">
+                    </div>
+                </div>
+                <div class="flow-cnt__box--text-box">
+                    <p class="flow-cnt__box--text-box--text">作りたい内容をヒアリングし、お見積もりをご確認いただきます</p>
+                </div>
+            </div>
+            <div class="flow-cnt__box">
+                <div class="flow-cnt__box--img-box">
+                    <div class="flow-cnt__box--img-box--size">
+                        <img class="flow-cnt__box--img-box--size--img" src="<?php echo get_stylesheet_directory_uri(); ?>/asset/images/flow-img/flow3.png" alt="">
+                    </div>
+                </div>
+                <div class="flow-cnt__box--text-box">
+                    <p class="flow-cnt__box--text-box--text">文章・画像などのご準備いただく物の有無の確認をします。</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="flow__cnt--line"></div>
+
+        <div class="flow-cnt">
+            <div class="flow-cnt__box">
+                <div class="flow-cnt__box--img-box">
+                    <div class="flow-cnt__box--img-box--size">
+                        <img class="flow-cnt__box--img-box--size--img" src="<?php echo get_stylesheet_directory_uri(); ?>/asset/images/flow-img/flow4.png" alt="">
+                    </div>
+                </div>
+                <div class="flow-cnt__box--text-box">
+                    <p class="flow-cnt__box--text-box--text">制作スタート<br>デザイン提案 ->修正 -> サイト構築->微調整</p>
+                </div>
+            </div>
+            <div class="flow-cnt__box">
+                <div class="flow-cnt__box--img-box">
+                    <div class="flow-cnt__box--img-box--size">
+                        <img class="flow-cnt__box--img-box--size--img" src="<?php echo get_stylesheet_directory_uri(); ?>/asset/images/flow-img/flow5.png" alt="">
+                    </div>
+                </div>
+                <div class="flow-cnt__box--text-box">
+                    <p class="flow-cnt__box--text-box--text">ズレや不十分な点がないかの最終チェックをします</p>
+                </div>
+            </div>
+            <div class="flow-cnt__box">
+                <div class="flow-cnt__box--img-box">
+                    <div class="flow-cnt__box--img-box--size">
+                        <img class="flow-cnt__box--img-box--size--img" src="<?php echo get_stylesheet_directory_uri(); ?>/asset/images/flow-img/flow6.png" alt="">
+                    </div>
+                </div>
+                <div class="flow-cnt__box--text-box">
+                    <p class="flow-cnt__box--text-box--text">納品形式はZipファイルもしくはWordpressに直接 アップロードします。</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="works">
+    <div class="works__title">
+        <div class="title">
+            <h2 class="title__main-text">制作実績</h2>
+            <div class="title__line"></div>
+            <p class="title__sub-text">WORKS</p>
+        </div>
+    </div>
+    <div></div>
+</section>
 <?php get_footer(); ?>
